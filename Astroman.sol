415f8bf3a5bf2f4886760aed56a32c598d8ad4f4
Astroman <82848569+AstromanGlobal@users.noreply.github.com>
2021-05-08 11:48:13 +0300

    Update Astroman.sol

--- Astroman.sol
+++ Astroman.sol
@@ -721,7 +721,7 @@ contract Astroman is Context, IERC20, Ownable {
     constructor () public {
         _rOwned[_msgSender()] = _rTotal;
         
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ed43c718714eb63d5aa57b78b54704e256024e);
 
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
